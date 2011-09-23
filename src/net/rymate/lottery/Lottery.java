6fe12bc5b8d75f7012739cf2918f502c311fc664
rymate1234 <supermarioryan@gmail.com>
2011-09-23 20:57:00 +0100

    First commit

---
+++ src/net/rymate/lottery/Lottery.java
@@ -0,0 +1,55 @@
+/*
+ * Lottery - by rymate
+ * http://
+ *
+ * powered by Kickstarter
+ */
+
+package net.rymate.lottery;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+
+public class Lottery extends JavaPlugin{
+	private Logger log;
+	private PluginDescriptionFile description;
+
+	private String prefix;
+
+	
+	@Override
+	public void onEnable(){
+		log = Logger.getLogger("Minecraft");
+		description = getDescription();
+		prefix = "["+description.getName()+"] ";
+
+		log("loading "+description.getFullName());
+
+
+		
+
+		
+		getCommand("/lottery").setExecutor(new LotteryCommand(this));
+		getCommand("/reloadlottery").setExecutor(new LotteryReloadCommand(this));
+
+		
+
+	}
+	
+	@Override
+	public void onDisable(){
+		log("disabled "+description.getFullName());
+
+	}
+	public void log(String message){
+		log.info(prefix+message);
+	}
+
+
+
+	
+
+}
