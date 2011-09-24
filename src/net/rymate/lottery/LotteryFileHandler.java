167cb34c5285fd47124e36bc4f79e2c7ce865120
rymate1234 <supermarioryan@gmail.com>
2011-09-24 07:54:02 +0100

    Done stuff with the config! It should auto generate a sample now :D Now need to figure out how the hell I'm going to load it

--- src/net/rymate/lottery/LotteryFileHandler.java
+++ src/net/rymate/lottery/LotteryFileHandler.java
@@ -1,5 +1,9 @@
 package net.rymate.lottery;
 
+import org.bukkit.util.config.Configuration;
+
+import java.io.File;
+
 /**
  * Created by IntelliJ IDEA.
  * User: rymate
@@ -10,12 +14,24 @@ package net.rymate.lottery;
 public class LotteryFileHandler {
 
     private Lottery plugin;
+    private final Configuration c;
+    private final File lotteryFile;
 
     public LotteryFileHandler(Lottery lottery) {
         this.plugin = lottery;
+        this.lotteryFile = new File("plugins/BlockLottery/lottery.yml");
+        this.c = new Configuration(lotteryFile);
     }
 
     public void loadConfig() {
+        if (!lotteryFile.exists()) {
+            plugin.log("Config wasn't found! Generating sample!");
+            c.setProperty("lottery.sample.chance", "50");
+            c.setProperty("lottery.sample.cost", "4:256, 1:256");
+            c.setProperty("lottery.sample.prizes", "15:32, 1:10, 64:10");
+        } else {
+            //initiate standard loading
+        }
         //stuff will go here :D
     }
 }
